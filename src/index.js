3cb5d7d992651eaa8df2730bf83f9d4a0d0488b3
unnhost <rinkovnsk@gmail.com>
2025-03-31 04:33:42 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import IQTestApp from "./IQTestApp";
+import AppWrapper from "./AppWrapper";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <IQTestApp />
-  </React.StrictMode>
-);
+function App() {
+  return <AppWrapper />;
+}
+
+export default App;
