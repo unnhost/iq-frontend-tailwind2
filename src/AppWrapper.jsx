a00b3f08364bb63ef6576f3ddd025d44e830b95a
unnhost <rinkovnsk@gmail.com>
2025-03-31 17:55:38 -0400

    Add files via upload

--- src/AppWrapper.jsx
+++ src/AppWrapper.jsx
@@ -1,22 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import IQTestApp from "./IQTestApp";
-import Leaderboard from "./Leaderboard";
 
 const AppWrapper = () => {
-  const [showLeaderboard, setShowLeaderboard] = useState(false);
-
   return (
     <div className="min-h-screen bg-gray-100">
-      <div className="text-center p-4">
-        <button 
-          className="px-4 py-2 bg-blue-600 text-white rounded-lg shadow"
-          onClick={() => setShowLeaderboard(!showLeaderboard)}
-        >
-          {showLeaderboard ? "Back to IQ Test" : "🏆 Show Leaderboard"}
-        </button>
-      </div>
-
-      {showLeaderboard ? <Leaderboard /> : <IQTestApp />}
+      <IQTestApp />
     </div>
   );
 };
